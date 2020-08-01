6e221d8c442e2e15b7d5510c25659f3c015c15b0
SidharthaRay <sidhartha.ray533@gmail.com>
2020-08-01 17:03:27 +0530

    Adding PySpark code

---
+++ Continuing with RDD.py
@@ -0,0 +1,316 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ** Example 2.1: Pair RDDs - groupByKey **
+
+# COMMAND ----------
+
+txn_rdd = sc.textFile("/FileStore/tables/cred_txn.csv", 5)
+txn_rdd.take(5)
+
+# COMMAND ----------
+
+acc_txncat_rdd = txn_rdd \
+  .filter(lambda rec: 'AccNum' not in rec) \
+  .map(lambda rec: rec.split('~')) \
+  .map(lambda rec: (rec[0], rec[3])) \
+  .groupByKey() \
+  .map(lambda rec: (rec[0], list(rec[1])))
+
+acc_txncat_rdd.take(5)
+
+
+# COMMAND ----------
+
+acc_txncat_rdd \
+  .map(lambda rec: (rec[0], set(rec[1]))) \
+  .take(5)
+
+# COMMAND ----------
+
+acc_txncat_rdd \
+  .filter(lambda rec: 'Movies' in rec[1]) \
+  .take(5)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ** Example 2.2: Pair RDDs - reduceByKey **
+
+# COMMAND ----------
+
+txn_rdd = sc.textFile('/FileStore/tables/cred_txn.csv', 5)
+
+acc_txncat_rdd = txn_rdd \
+  .filter(lambda rec: 'AccNum' not in rec) \
+  .map(lambda rec: rec.split('~')) \
+  .map(lambda rec: (rec[0], float(rec[1])))
+
+acc_txncat_rdd \
+  .reduceByKey(lambda amt1, amt2: amt1 + amt2) \
+  .take(5)
+
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ** Example 2.3: Pair RDDs - aggregateByKey **
+
+# COMMAND ----------
+
+txn_rdd = sc.textFile('/FileStore/tables/cred_txn.csv', 5)
+
+acc_txncat_rdd = txn_rdd \
+  .filter(lambda rec: 'AccNum' not in rec) \
+  .map(lambda rec: rec.split('~')) \
+  .map(lambda rec: (rec[0], (rec[3], float(rec[1]))))
+
+
+# COMMAND ----------
+
+acc_txncat_rdd \
+  .reduceByKey(lambda catAmt1, catAmt2: (catAmt1[1] if(catAmt1[1] > catAmt2[1]) else catAmt2[1])) \
+  .take(5)
+
+
+# COMMAND ----------
+
+acc_txncat_rdd \
+  .reduceByKey(lambda catAmt1, catAmt2: catAmt1 if(catAmt1[1] > catAmt2[1]) else catAmt2) \
+  .take(5)
+
+
+# COMMAND ----------
+
+import sys
+
+# Sequence operation : Finding Maximum Amount from a single partition
+def sumAtPartition(acc, element):
+  if(acc > element[1]): 
+    return acc 
+  else: 
+    return element[1]
+
+# Combiner Operation : Finding Maximum Amount out Partition-Wise Accumulators
+def sumAcrossPartition(acc1, acc2):
+  if(acc1 > acc2): 
+    return acc1 
+  else: 
+    return acc2
+
+acc_txncat_rdd \
+  .aggregateByKey(sys.float_info.min, sumAtPartition, sumAcrossPartition) \
+  .collect()
+
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ** Example 2.4: Pair RDDs - aggregateByKey (continued..) **
+
+# COMMAND ----------
+
+student_rdd = sc.parallelize( \
+  [
+    ("Ravi", "Maths", 83), 
+    ("Ravi", "Physics", 74), 
+    ("Ravi", "Chemistry", 91), 
+    ("Ravi", "Biology", 82), 
+    ("Kiran", "Maths", 69), 
+    ("Kiran", "Physics", 62), 
+    ("Kiran", "Chemistry", 97), 
+    ("Kiran", "Biology", 80), 
+    ("Rajesh", "Maths", 78), 
+    ("Rajesh", "Physics", 73), 
+    ("Rajesh", "Chemistry", 68), 
+    ("Rajesh", "Biology", 87), 
+    ("Raghu", "Maths", 87), 
+    ("Raghu", "Physics", 93), 
+    ("Raghu", "Chemistry", 91), 
+    ("Raghu", "Biology", 74), 
+    ("Dharan", "Maths", 56), 
+    ("Dharan", "Physics", 65), 
+    ("Dharan", "Chemistry", 71), 
+    ("Dharan", "Biology", 68), 
+    ("Jyoti", "Maths", 86), 
+    ("Jyoti", "Physics", 62), 
+    ("Jyoti", "Chemistry", 75), 
+    ("Jyoti", "Biology", 83), 
+    ("Ankita", "Maths", 63), 
+    ("Ankita", "Physics", 69), 
+    ("Ankita", "Chemistry", 64), 
+    ("Ankita", "Biology", 60)],  
+  3)
+ 
+
+# COMMAND ----------
+
+# Sequence operation : Finding Maximum Marks from a single partition
+def seqOperation(acc, element): 
+  if(acc > element[1]): 
+    return acc 
+  else: 
+    return element[1]
+
+# Combiner Operation : Finding Maximum Marks out Partition-Wise Accumulators
+def combOperation(acc1, acc2): 
+  if(acc1 > acc2): 
+    return acc1 
+  else: 
+    return acc2
+
+# Zero Value: Zero value in our case will be 0 as we are finding Maximum Marks
+student_rdd \
+  .map(lambda student: (student[0], (student[1], student[2]))) \
+  .aggregateByKey(0, seqOperation, combOperation) \
+  .collect()
+ 
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ** Example 2.5: Pair RDDs - mapValues **
+
+# COMMAND ----------
+
+txn_rdd = sc.textFile('/FileStore/tables/cred_txn.csv', 5)
+
+acc_txnamt_rdd = txn_rdd \
+  .filter(lambda rec: 'AccNum' not in rec) \
+  .map(lambda rec: rec.split('~')) \
+  .map(lambda rec: (rec[0], float(rec[1])))
+
+acc_txnamt_rdd \
+  .reduceByKey(lambda amt1, amt2: amt1 + amt2) \
+  .mapValues(lambda amt: (amt, 1)) \
+  .reduceByKey(lambda amt1, amt2: (amt1[0] + amt2[0], amt1[1] + amt2[1])) \
+  .mapValues(lambda rec: rec[0]/rec[1]) \
+  .take(5)
+
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ** Example 2.6: Pair RDDs - countByKey **
+
+# COMMAND ----------
+
+txn_rdd = sc.textFile('/FileStore/tables/cred_txn.csv', 5)
+
+txn_rdd \
+  .filter(lambda rec: 'AccNum' not in rec) \
+  .map(lambda rec: rec.split('~')) \
+  .map(lambda rec: (rec[0], float(rec[1]))) \
+  .countByKey()
+
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ** Example 2.7: Joins - inner join **
+
+# COMMAND ----------
+
+txn_rdd = sc.textFile('/FileStore/tables/cred_txn.csv', 5) \
+  .filter(lambda rec: 'AccNum' not in rec) \
+  .map(lambda rec: rec.split('~')) \
+  .map(lambda rec: (rec[0], float(rec[1]))) \
+
+txn_rdd.take(5)
+
+# COMMAND ----------
+
+raw_cust_rdd = sc.textFile('/FileStore/tables/cust_name.csv', 1)
+raw_cust_rdd.take(5)
+
+
+# COMMAND ----------
+
+cust_rdd = raw_cust_rdd \
+  .filter(lambda rec: 'AccNum' not in rec) \
+  .map(lambda rec: rec.split('~')) \
+  .map(lambda rec: (rec[0], rec[1]))
+
+cust_rdd.take(5)
+
+
+# COMMAND ----------
+
+tracked_customers = txn_rdd.join(cust_rdd)
+tracked_customers.take(5)
+
+# COMMAND ----------
+
+tracked_customers.map(lambda rec: rec[0]).distinct().collect()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ** Example 2.8: Joins - outer join **
+
+# COMMAND ----------
+
+# Left outer join
+tracked_customers = txn_rdd.leftOuterJoin(cust_rdd)
+tracked_customers.map(lambda rec: rec[0]).distinct().collect()
+
+
+# COMMAND ----------
+
+tracked_customers = txn_rdd.fullOuterJoin(cust_rdd)
+tracked_customers.map(lambda rec: rec[0]).distinct().collect()
+
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ** Example 2.9: Accumulator **
+
+# COMMAND ----------
+
+last_years_logs = [
+  '2015-09-01 10:00:01|Error|Ac #3211001 ATW 10000 INR', 
+  '2015-09-02 10:00:07|Info|Ac #3281001 ATW 11000 INR',
+  '2015-10-01 10:00:09|error|Ac #3311001 AWT 10500 INR', 
+  '2015-11-01 10:00:01|error|Ac #3211001 AWT 10000 INR',
+  '2016-09-01 10:00:01|info|Ac #3211001 AWT 5000 INR', 
+  '2016-09-02 10:00:01|ERROR|Ac #3211001 AWT 10000 INR',
+  '2016-10-01 10:00:01|error|Ac #3211001 AWT 8000 INR', 
+  '2016-11-01 10:00:01|error|Ac #3211001 AWT 10000 INR',
+  '2016-12-01 10:00:01|Error|Ac #8211001 AWT 80000 INR', 
+  '2016-12-02 10:00:01|error|Ac #9211001 AWT 90000 INR',
+  '2016-12-10 10:00:01|error|Ac #3811001 AWT 15000 INR', 
+  '2016-12-01 10:00:01|info|Ac #3219001 AWT 16000 INR'
+]
+last_years_logs_rdd = sc.parallelize(last_years_logs)
+
+
+# COMMAND ----------
+
+error_log_lines = 0
+last_years_logs_rdd.foreach(lambda log: ((error_log_lines + 1) if('ERROR' in log.upper()) else error_log_lines))
+print(error_log_lines)
+
+
+# COMMAND ----------
+
+error_log_lines = sc.accumulator(0)
+last_years_logs_rdd.foreach(lambda log: (error_log_lines.add(1) if('ERROR' in log.upper()) else error_log_lines.add(0)))
+error_log_lines.value
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ** Example 2.10: Broadcast Variable **
+
+# COMMAND ----------
+
+broadcast_var = sc.broadcast(2)
+ 
+input_rdd = sc.parallelize([1, 2, 3])
+added_rdd = input_rdd.map(lambda num: broadcast_var.value + num)
+ 
+added_rdd.collect()
+
+# COMMAND ----------
+
+
---
+++ Starting with RDD.py
@@ -0,0 +1,375 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ** Example 1.1 - Word count example **
+
+# COMMAND ----------
+
+text_rdd = sc.textFile('/FileStore/tables/SampleText.txt') # RDD of string
+splitted_rdd = text_rdd.flatMap(lambda line: line.split(' ')) # RDD of list of string
+word_pair_rdd = splitted_rdd.map(lambda word: (word, 1))  # RDD of tuple (string, int)
+word_count_rdd = word_pair_rdd.reduceByKey(lambda cnt1, cnt2: cnt1 + cnt2) # RDD of tuple (string, int)
+word_count_rdd.take(5) # list of tuple (string, int)
+
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ** Example 1.2 - Creating RDD with parallelize() function **
+
+# COMMAND ----------
+
+num_rdd = sc.parallelize(range(1, 11))  # RDD of int
+num_rdd.getNumPartitions() # int
+
+# COMMAND ----------
+
+sc.defaultParallelism
+
+# COMMAND ----------
+
+num_rdd_parts = num_rdd.glom()
+num_rdd_parts.collect()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ** Example 1.3 - repartition() and coalesce() function **
+
+# COMMAND ----------
+
+bigger_rdd = sc.parallelize(range(1, 1001), 8)
+bigger_rdd.take(3)
+
+# COMMAND ----------
+
+bigger_rdd.getNumPartitions()
+
+# COMMAND ----------
+
+bigger_rdd.count()
+
+# COMMAND ----------
+
+bigger_rdd.glom().map(lambda rec: len(rec)).collect()
+
+# COMMAND ----------
+
+bigger_rdd.repartition(10).saveAsTextFile('/FileStore/tables/biggerRdd/ten13')
+sc.textFile('/FileStore/tables/biggerRdd/ten13').getNumPartitions()
+
+
+# COMMAND ----------
+
+bigger_rdd.repartition(10).glom().map(lambda rec: len(rec)).collect()
+
+# COMMAND ----------
+
+bigger_rdd.repartition(5).saveAsTextFile('/FileStore/tables/biggerRdd/four14')
+sc.textFile('/FileStore/tables/biggerRdd/four14').getNumPartitions()
+
+
+# COMMAND ----------
+
+bigger_rdd.repartition(5).glom().map(lambda rec: len(rec)).collect()
+
+# COMMAND ----------
+
+bigger_rdd.coalesce(6).saveAsTextFile('dbfs:/FileStore/tables/biggerRdd/coalesce2')
+sc.textFile('/FileStore/tables/biggerRdd/coalesce2').getNumPartitions()
+
+# Homework: Is the number of records same in each of the partitions for both repartition() and coalesce()? Justify the full shufling meaning!
+
+
+# COMMAND ----------
+
+bigger_rdd.coalesce(6).glom().map(lambda rec: len(rec)).collect()
+
+# COMMAND ----------
+
+txn_list = ['txn01~1000', 'txn02~2000', 'txn03~1000', 'txn04~2000']
+txn_rdd = sc.parallelize(txn_list)
+
+def strSplit(line):
+  return line.split('~')
+splitted_txn_rdd = txn_rdd.map(strSplit)
+
+def getAmount(rec):
+  return float(rec[1])
+txn_amt_rdd = splitted_txn_rdd.map(getAmount)
+
+def sum(amt1, amt2):
+  return (amt1 + amt2)
+txn_amt_rdd.reduce(sum)
+
+
+# COMMAND ----------
+
+txn_list = ['txn01~1000', 'txn02~2000', 'txn03~1000', 'txn04~2000']
+txn_rdd = sc.parallelize(txn_list)
+
+splitted_txn_rdd = txn_rdd.map(lambda rec: rec.split('~'))
+txn_amt_rdd = splitted_txn_rdd.map(lambda rec: float(rec[1]))
+txn_amt_rdd.reduce(lambda amt1, amt2: amt1 + amt2)
+
+
+# COMMAND ----------
+
+server_logs = [
+  '2015-09-01 10:00:01|Error|Ac #3211001 ATW 10000 INR', 
+  '2015-09-02 10:00:07|Info|Ac #3281001 ATW 11000 INR',
+  '2015-10-01 10:00:09|error|Ac #3311001 AWT 10500 INR', 
+  '2015-11-01 10:00:01|error|Ac #3211001 AWT 10000 INR',
+  '2016-09-01 10:00:01|info|Ac #3211001 AWT 5000 INR', 
+  '2016-09-02 10:00:01|ERROR|Ac #3211001 AWT 10000 INR',
+  '2016-10-01 10:00:01|error|Ac #3211001 AWT 8000 INR', 
+  '2016-11-01 10:00:01|error|Ac #3211001 AWT 10000 INR',
+  '2016-12-01 10:00:01|Error|Ac #8211001 AWT 80000 INR', 
+  '2016-12-02 10:00:01|error|Ac #9211001 AWT 90000 INR',
+  '2016-12-10 10:00:01|error|Ac #3811001 AWT 15000 INR', 
+  '2016-12-01 10:00:01|info|Ac #3219001 AWT 16000 INR'
+]
+
+logs_rdd = sc.parallelize(server_logs)
+
+logs_rdd.filter(lambda log: '2016-12' in log).count()
+
+
+# COMMAND ----------
+
+logs_rdd \
+  .map(lambda log: log.lower()) \
+  .filter(lambda log: '2016-12' in log and 'error' in log) \
+  .count()
+
+
+# COMMAND ----------
+
+# Benefits of Laziness for Large - Scale Data
+# Tungsten Optimizer
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ** Example 1.4 - Saving data as ObjectFile, SequenceFile and NewAPIHadoopFile **
+
+# COMMAND ----------
+
+reg_rdd = sc.textFile('/FileStore/tables/KC_Extract_1_20171009.csv') \
+  .map(lambda rec: rec.split('|', -1))
+
+reg_rdd.take(3)
+
+
+# COMMAND ----------
+
+reg_rdd.saveAsTextFile('/FileStore/tables/Reg_v12/')
+sc.textFile('dbfs:/FileStore/tables/Reg_v12/').take(3)
+
+
+# COMMAND ----------
+
+reg_rdd.map(lambda rec: (rec[4], rec[10])).saveAsSequenceFile('dbfs:/FileStore/tables/Reg_v43')
+sc.sequenceFile('dbfs:/FileStore/tables/Reg_v43').take(5)
+
+
+# COMMAND ----------
+
+reg_rdd.map(lambda record: (record[2], record[10])) \
+.saveAsNewAPIHadoopFile("dbfs:/FileStore/tables/Reg_v53/", \
+                         'org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat', \
+                         'org.apache.hadoop.io.Text', \
+                         'org.apache.hadoop.io.Text'
+                         )
+
+
+# COMMAND ----------
+
+sc.newAPIHadoopFile("/FileStore/tables/Reg_v53",
+                   'org.apache.hadoop.mapreduce.lib.input.SequenceFileInputFormat',
+                   'org.apache.hadoop.io.Text', \
+                   'org.apache.hadoop.io.Text') \
+  .take(3)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ** Example 1.5: Set Operations **
+
+# COMMAND ----------
+
+et_log = [
+  "2017-10-13 10:00:00|RIL|830.00",
+  "2017-10-13 10:00:10|MARUTI SUZUKI|8910.00",
+  "2017-10-13 10:00:20|RIL|835.00",
+  "2017-10-13 10:00:30|MARUTI SUZUKI|8890.00"
+]
+et_log_rdd = sc.parallelize(et_log)
+
+mc_log = [
+  "2017-10-13 10:00:20|RIL|835.00",
+  "2017-10-13 10:00:40|MARUTI SUZUKI|8870.00"
+]
+mc_log_rdd = sc.parallelize(mc_log)
+
+
+# COMMAND ----------
+
+# Union - Share price example
+print('Total number of stocks: {0}'.format(et_log_rdd.union(mc_log_rdd).distinct().count()))
+et_log_rdd.union(mc_log_rdd).distinct().collect()
+
+
+# COMMAND ----------
+
+# Intersection
+et_log_rdd.intersection(mc_log_rdd).collect()
+
+
+# COMMAND ----------
+
+# Set difference
+et_log_rdd.subtract(mc_log_rdd).collect()
+
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ** Example 1.6: Other Useful RDD Actions **
+
+# COMMAND ----------
+
+num_rdd = sc.parallelize(range(0, 11))
+num_rdd.takeSample(False, 5)
+
+
+# COMMAND ----------
+
+num_rdd.takeSample(True, 5)
+
+
+# COMMAND ----------
+
+num_rdd.takeSample(False, 5, 100)
+
+
+# COMMAND ----------
+
+sc.parallelize([10, 1, 2, 9, 3, 4, 5, 6, 7]).takeOrdered(6)
+
+
+# COMMAND ----------
+
+sc.parallelize([10, 1, 2, 9, 3, 4, 5, 6, 7]).takeOrdered(6, lambda x: -x)
+
+
+# COMMAND ----------
+
+sc.parallelize([(10, 1), (2, 9), (3, 4), (5, 6), (2, 7)]).takeOrdered(6, lambda x: -x[0])
+
+
+# COMMAND ----------
+
+sc.parallelize([(10, 1), (2, 9), (3, 4), (5, 6), (2, 7)]).takeOrdered(6, lambda x: (-x[0], -x[1]))
+
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ** Example 1.7: Caching and Persistence **
+
+# COMMAND ----------
+
+from pyspark import StorageLevel
+server_logs = [
+  "2015-09-01 10:00:01|Error|Ac #3211001 ATW 10000 INR", 
+  "2015-09-02 10:00:07|Info|Ac #3281001 ATW 11000 INR",
+  "2015-10-01 10:00:09|error|Ac #3311001 AWT 10500 INR", 
+  "2015-11-01 10:00:01|error|Ac #3211001 AWT 10000 INR",
+  "2016-09-01 10:00:01|info|Ac #3211001 AWT 5000 INR", 
+  "2016-09-02 10:00:01|ERROR|Ac #3211001 AWT 10000 INR",
+  "2016-10-01 10:00:01|error|Ac #3211001 AWT 8000 INR", 
+  "2016-11-01 10:00:01|error|Ac #3211001 AWT 10000 INR",
+  "2016-12-01 10:00:01|Error|Ac #8211001 AWT 80000 INR", 
+  "2016-12-02 10:00:01|error|Ac #9211001 AWT 90000 INR",
+  "2016-12-10 10:00:01|error|Ac #3811001 AWT 15000 INR", 
+  "2016-12-01 10:00:01|info|Ac #3219001 AWT 16000 INR"
+]
+server_logs_rdd = sc.parallelize(server_logs)
+logs_with_errors = server_logs_rdd \
+  .map(lambda log: log.lower()) \
+  .filter(lambda log: 'error' in log) \
+  .persist(StorageLevel.MEMORY_ONLY_SER) 
+
+logs_with_errors.take(2)
+
+
+# COMMAND ----------
+
+logs_with_errors.count()   # faster
+
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ** Example 1.8: Understanding cluster topology Example **
+
+# COMMAND ----------
+
+people = [
+  "Sidharth,32",
+  "Atul,33",
+  "Sachin,30"
+]
+people_rdd = sc.parallelize(people) \
+  .map(lambda line: line.split(',')) 
+
+
+# COMMAND ----------
+
+people_rdd \
+  .foreach(lambda rec: print('{0}, {1}'.format(rec[0], rec[1])))
+
+
+# COMMAND ----------
+
+people_rdd.take(2)
+
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ** Example 1.10: mapPartitions() example **
+
+# COMMAND ----------
+
+def strSplit(partition):
+  sec_iterator = []
+  for rec in partition:
+    sec_iterator.append(rec.split('|'))
+  return sec_iterator
+      
+def getAmount(partition):
+  sec_iterator = []
+  for rec in partition:
+    sec_iterator.append(float(rec[2]))
+  return sec_iterator
+
+sc.textFile('/FileStore/tables/txn_fct.csv') \
+  .filter(lambda rec: 'amount' not in rec) \
+  .mapPartitions(strSplit) \
+  .mapPartitions(getAmount) \
+  .reduce(lambda amt1, amt2: amt1 + amt2)
+
+
+# COMMAND ----------
+
+sc.textFile('/FileStore/tables/txn_fct.csv') \
+  .filter(lambda rec: 'amount' not in rec) \
+  .mapPartitions(lambda partition: (rec.split('|') for rec in partition)) \
+  .mapPartitions(lambda partition: (float(rec[2]) for rec in partition)) \
+  .reduce(lambda amt1, amt2: amt1 + amt2)
+
+
+# COMMAND ----------
+
+
